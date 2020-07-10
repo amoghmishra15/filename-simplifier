28baecaa46527f3fec66f5a3b2495ad5d6eb8e00
Az-21 <abhishekc55@gmail.com>
2020-07-10 14:51:44 +0530

    [*] removal, (*) removal, '-' removal, '.' removal, smart titlecase

--- simplify.py
+++ simplify.py
@@ -2,50 +2,87 @@ import os
 import re
 import json
 from settings import changeSettings
+from configCreator import makeConfig
 
+# --------------------------------------------------------------------------------------------------
+#   Functions
+# --------------------------------------------------------------------------------------------------
 
-#   Clear terminal
+#       1. Clear terminal.
 def clearTerminal():
 	os.system('cls' if os.name == 'nt' else 'clear')
 
 
-#   Load settings
+#       2. Load/create settings.
+if os.path.isfile('config.json') is False:
+	makeConfig()
+
 with open('config.json') as f:
 	config = json.load(f)
 
 
-#   Terminate the program on invalid input
+#       3. Terminate the program on invalid input.
 def terminate():
 	print('\nTerminating the program. No changes were made.')
 	print('----------------------------------------------')
-	quit()
+	input('Press [Enter] to exit...')
+	exit()
 
 
+#       4. Function to convert space separated string to tuple.
+def listify(stringList):
+	stringList = " ".join(stringList.split())
+	stringList = stringList.split(' ')
+	return tuple(stringList)
+
+
+# --------------------------------------------------------------------------------------------------
 #   Load settings
+# --------------------------------------------------------------------------------------------------
 extNumber = int(config['flags'][0]['selectedExtension'])
-selectedExtensions = tuple(eval(config['extensions'][extNumber]['value']))
+selectedExtensions = listify(config['extensions'][extNumber]['value'])
+defaultBlacklist = listify(config['defaultBlacklist'])
+customBlacklist = listify(config['customBlacklist'])
+
+rmSquareBrackets = config['flags'][0]['rmSquareBracket']
+rmParentheses = config['flags'][0]['rmParentheses']
+rmDash = config['flags'][0]['rmDash']
 rmForeign = config['flags'][0]['rmForeign']
-defaultBlacklist = tuple(eval(config['defaultBlacklist']))
+
 enDefaultBlacklist = config['flags'][0]['enDefaultBlacklist']
-customBlacklist = tuple(eval(config['customBlacklist']))
 enCustomBlacklist = config['flags'][0]['enCustomBlacklist']
 enTitleCase = config['flags'][0]['enTitleCase']
 
+
+# --------------------------------------------------------------------------------------------------
 #   Print settings
+# --------------------------------------------------------------------------------------------------
 clearTerminal()
 print('\nFilename Simplifier')
 print('----------------------------')
 
-print('Simplify filenames ending with: {}'.format(selectedExtensions))
+#       1. Extensions to work on.
+if extNumber == 4:
+	print('Simplify every file and folder')
+else:
+	print('Simplify filenames ending with: {}'.format(selectedExtensions))
+
 if enCustomBlacklist is True:
 	print('Custom blacklisted words: {}'.format(customBlacklist))
 
-print('\nRemove non-English characters: {}'.format(rmForeign))
-print('Capitalize each word: {}'.format(enTitleCase))
-print('Default blacklisted words removal enabled: {}'.format(enDefaultBlacklist))
+#       2. Flags
+print('\nRemove [text inside square brackets]: {}'.format(rmSquareBrackets))
+print('Remove (text inside parentheses): {}'.format(rmParentheses))
+print('Remove dashes-between-words: {}'.format(rmDash))
+print('Remove non-English characters: {}'.format(rmForeign))
+print('Smart capitalize (): {}'.format(enTitleCase))
+print('\nDefault blacklisted words removal enabled: {}'.format(enDefaultBlacklist))
 print('Custom blacklisted words removal enabled: {}'.format(enCustomBlacklist))
 
+
+# --------------------------------------------------------------------------------------------------
 #   Edit settings prompt
+# --------------------------------------------------------------------------------------------------
 print('\nSettings loaded successfully')
 print('----------------------------')
 userIn = input('Edit settings? (y/n): ')
@@ -55,9 +92,13 @@ if userIn.lower() == 'y':
 	print('Settings updated')
 	print('----------------')
 	print('Please rerun the program')
-	quit()
+	input('Press [Enter] to exit...')
+	exit()
 
+
+# --------------------------------------------------------------------------------------------------
 #   User input: working directory
+# --------------------------------------------------------------------------------------------------
 clearTerminal()
 print('Enter the full directory/path of the folder which contains files to be renamed')
 rnDir = input('dir: ')
@@ -66,7 +107,10 @@ rnDir = input('dir: ')
 clearTerminal()
 print('\nProcessing all files ending with {} in "{}"'.format(selectedExtensions, rnDir))
 
+
+# --------------------------------------------------------------------------------------------------
 #   Change to working directory and list files
+# --------------------------------------------------------------------------------------------------
 os.chdir(rnDir)
 print('\nFollowing files will be affected')
 print('--------------------------------')
@@ -76,16 +120,25 @@ for f in os.listdir():
 	if fExt.endswith(selectedExtensions):
 		print(fExt)
 
+
+# --------------------------------------------------------------------------------------------------
 #   User input: confirm working directory
+# --------------------------------------------------------------------------------------------------
 prompt = input('\nContinue? (y/n): ')
 if prompt.lower() == 'y':
 	print('', end='')
 else:
 	terminate()
 
-regexBrackets = r'\[(.*?)\]'  # [*]
-regexBitrate = r'(?i)\d\d\dkbps'  # 320kbps
+
+# --------------------------------------------------------------------------------------------------
+#   Regex
+# --------------------------------------------------------------------------------------------------
+regexBrackets = r'\[(.*?)\]'        # [*]
+regexParentheses = r'\((.*?)\)'     # (*)
+regexBitrate = r'(?i)\d\d\dkbps'    # 320kbps
 regexUnderscore = r'_'
+regexUppercase = r'[A-Z]'
 
 # NOTE: program calls A->B->C->D. If, say, typeB is called first, it will break the regex for typeA.
 regexResolutionTypeB = r'(?i)\d\d\dp'  # 720p
@@ -93,17 +146,32 @@ regexResolutionTypeA = r'(?i)\d\d\d\dp'  # 1080p
 regexResolutionTypeD = r'(?i)\d\d\dx\d\d\d'  # 720x480
 regexResolutionTypeC = r'(?i)\d\d\d\dx\d\d\d\d'  # 1920x1080
 
-# Misc
+
+# --------------------------------------------------------------------------------------------------
+#   Misc
+# --------------------------------------------------------------------------------------------------
 allRenames = []                         # Store all rename previews. Used to check for duplicates.
 
+
+# --------------------------------------------------------------------------------------------------
 # Core function
+# --------------------------------------------------------------------------------------------------
 def desalinate(mode):
 	for file in os.listdir():
 		newF = file
 		if newF.endswith(selectedExtensions):
 
 			# Regex removers
-			newF = re.sub(regexBrackets, " ", newF)
+			if rmSquareBrackets is True:
+				newF = re.sub(regexBrackets, " ", newF)
+
+			if rmParentheses is True:
+				newF = re.sub(regexParentheses, " ", newF)
+
+			if rmDash is True:
+				newF = newF.replace('-', ' ')
+
+			# More regex removers
 			newF = re.sub(regexBitrate, " ", newF)
 			newF = re.sub(regexUnderscore, " ", newF)
 			newF = re.sub(regexResolutionTypeA, " ", newF)
@@ -121,14 +189,16 @@ def desalinate(mode):
 				newF = newF.encode("ascii", errors="ignore").decode()
 
 			# Default blacklist
-			for index, words in enumerate(defaultBlacklist):
-				regexBlacklist = "(?i)" + defaultBlacklist[index]
-				newF = re.sub(regexBlacklist, " ", newF)
+			if enDefaultBlacklist is True:
+				for index, words in enumerate(defaultBlacklist):
+					regexBlacklist = "(?i)" + defaultBlacklist[index]
+					newF = re.sub(regexBlacklist, " ", newF)
 
 			# Custom blacklist
-			for index, words in enumerate(customBlacklist):
-				regexBlacklist = "(?i)" + customBlacklist[index]
-				newF = re.sub(regexBlacklist, " ", newF)
+			if enCustomBlacklist is True:
+				for index, words in enumerate(customBlacklist):
+					regexBlacklist = "(?i)" + customBlacklist[index]
+					newF = re.sub(regexBlacklist, " ", newF)
 
 			# Remove extra whitespace
 			newF = newF.replace('( )', '')
@@ -141,10 +211,14 @@ def desalinate(mode):
 
 			# Title case
 			if enTitleCase is True:
-				newF = newF.split('.')
-				newF[0] = newF[0].title()
-				newF = '.'.join(newF)
+				newF = newF.split(' ')
+				for index, words in enumerate(newF):
+					if bool(re.search(regexUppercase, newF[index])) is False:
+						newF[index] = newF[index].title()
 
+				newF = ' '.join(newF)
+
+			# Working mode
 			if mode is False:
 				print(newF)
 				allRenames.append(newF)  # Safeguard for duplicates
@@ -152,13 +226,18 @@ def desalinate(mode):
 				os.rename(file, newF)
 
 
+# --------------------------------------------------------------------------------------------------
 #   Preview new filenames
+# --------------------------------------------------------------------------------------------------
 print('\nPreview of renames')
 print('------------------')
 workingMode = False  # preview mode
 desalinate(workingMode)
 
-# Safeguard for duplicates
+
+# --------------------------------------------------------------------------------------------------
+#   Safeguard for duplicates
+# --------------------------------------------------------------------------------------------------
 if len(allRenames) != len(set(allRenames)):
 	print('\n\nFatal error')
 	print('-----------')
@@ -167,7 +246,10 @@ if len(allRenames) != len(set(allRenames)):
 	print('You cannot proceed until you rename the conflicting files manually.')
 	terminate()
 
+
+# --------------------------------------------------------------------------------------------------
 #   User input: final confirmation
+# --------------------------------------------------------------------------------------------------
 print('\nFinalize')
 print('--------')
 print('All renamed files have been validated against overwrite conflicts.')
