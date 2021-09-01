521109cffba836b9b5b4de60078e55dd41b6be4f
amoghmishra15 <53436778+amoghmishra15@users.noreply.github.com>
2021-09-01 22:04:39 +0530

    Added Dash remover

--- c-sharp-cli/Program.cs
+++ c-sharp-cli/Program.cs
@@ -1,18 +1,26 @@
 ﻿/* --------- Input parameters --------- */
-string filename = "Land.of_the.Lustrous";
+string filename = "Land.of_the.Lustrous-";
 
 /* --------- Flags --------- */
 bool removeUnderscoreFlag = true;
+bool removeDashFlag = true;
 
 /* --------- Implementation --------- */
 if(removeUnderscoreFlag)
     filename = RemoveUnderscore(filename);
+
+if(removeDashFlag)
+    filename = RemoveDash(filename);
 
 // Final output
 Console.WriteLine(filename);
 
 /* --------- Functions --------- */
 // Remove underscore: `abc_def` -> `abc def`
-string RemoveUnderscore(string filename) {
+static string RemoveUnderscore(string filename) {
     return filename.Replace("_", " ");
+}
+// Remove dash: `abc-def` -> `abc def`
+static string RemoveDash(string filename) {
+    return filename.Replace("-", " ");
 }
