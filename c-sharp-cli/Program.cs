51d5a0e1ad5c4f85148f0e263d79908e5616c2a0
Az21 <39803478+Az-21@users.noreply.github.com>
2021-09-01 21:18:36 +0530

    🌟 architecture init

--- c-sharp-cli/Program.cs
+++ c-sharp-cli/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/* --------- Input parameters --------- */
+string filename = "Land.of_the.Lustrous";
 
-Console.WriteLine("Hello, World!");
+/* --------- Flags --------- */
+bool removeUnderscoreFlag = true;
+
+/* --------- Implementation --------- */
+if(removeUnderscoreFlag)
+    filename = RemoveUnderscore(filename);
+
+// Final output
+Console.WriteLine(filename);
+
+/* --------- Functions --------- */
+// Remove underscore: `abc_def` -> `abc def`
+string RemoveUnderscore(string filename) {
+    return filename.Replace("_", " ");
+}
