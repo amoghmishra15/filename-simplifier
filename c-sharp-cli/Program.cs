07066c4f9097331bb32baab4c37afe8c2a9e3f06
Az21 <39803478+Az-21@users.noreply.github.com>
2021-09-02 00:29:21 +0530

    🚀 added . () [] \s+ and cli-friendly options

--- c-sharp-cli/Program.cs
+++ c-sharp-cli/Program.cs
@@ -1,16 +1,39 @@
-﻿/* --------- Input parameters --------- */
-string filename = "Land.of_the.Lustrous-";
+﻿using System.Text.RegularExpressions;
+
+/* --------- Input parameters --------- */
+string filename = "Land.of_the.    Lustrous(2009)-[HEVC]";
+string cliSeparator = "-";
 
 /* --------- Flags --------- */
 bool removeUnderscoreFlag = true;
 bool removeDashFlag = true;
+bool removeDotFlag = true;
+bool removeCurvedBracketFlag = true;
+bool removeSquareBracketFlag = true;
+bool cliFriendlyFlag = false;
 
 /* --------- Implementation --------- */
+// Order insensitive operations
 if(removeUnderscoreFlag)
     filename = RemoveUnderscore(filename);
 
 if(removeDashFlag)
     filename = RemoveDash(filename);
+
+if(removeDotFlag)
+    filename = RemoveDot(filename);
+
+if(removeCurvedBracketFlag)
+    filename = RemoveCurvedBracket(filename);
+
+if(removeSquareBracketFlag)
+    filename = RemoveSquareBracket(filename);
+
+// Order sensitive operations
+filename = ReduceWhitespace(filename);
+
+if(cliFriendlyFlag)
+    filename = CliFriendlyConvert(filename, cliSeparator);
 
 // Final output
 Console.WriteLine(filename);
@@ -20,7 +43,34 @@ Console.WriteLine(filename);
 static string RemoveUnderscore(string filename) {
     return filename.Replace("_", " ");
 }
+
 // Remove dash: `abc-def` -> `abc def`
 static string RemoveDash(string filename) {
     return filename.Replace("-", " ");
+}
+
+// Remove dot: `abc-def` -> `abc def`
+static string RemoveDot(string filename) {
+    return filename.Replace(".", " ");
+}
+
+// Remove parentheses + text: `abc (def)` -> `abc  `
+static string RemoveCurvedBracket(string filename) {
+    return Regex.Replace(filename, @" ?\(.*?\)", " ");
+}
+
+// Remove square brackets + text: `abc [def]` -> `abc  `
+static string RemoveSquareBracket(string filename) {
+    return Regex.Replace(filename, @" ?\[.*?\]", " ");
+}
+
+// Remove 2+ and trailing whitespace: ` abc    def ` -> `abc def`
+static string ReduceWhitespace(string filename) {
+    filename = Regex.Replace(filename, @"\s+", " "); // 2+
+    return filename.Trim(' '); // trailing
+}
+
+// CLI friendly conversion: `abc def` -> `abc-def`
+static string CliFriendlyConvert(string filename, string cliSeparator) {
+    return filename.Replace(" ", cliSeparator);
 }
