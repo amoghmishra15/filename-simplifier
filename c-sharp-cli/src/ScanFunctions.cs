5f45a5078e41dd5c0d38c37d063016c55eccd627
Az21 <39803478+Az-21@users.noreply.github.com>
2021-09-05 20:00:26 +0530

    👷‍♂️ added invalid path exception handler

--- c-sharp-cli/src/ScanFunctions.cs
+++ c-sharp-cli/src/ScanFunctions.cs
@@ -2,6 +2,13 @@
 class Scan {
     // Crawl the directory to find files with required extension
     public static IEnumerable<string> Files(string path, string[] extension) {
+        // Invalid path check
+        if(!Directory.Exists(path)) {
+            Console.WriteLine($"Directory '{path}' does not exist. Exiting...");
+            System.Environment.Exit(1);
+        }
+
+        // Load files in the directory
         IEnumerable<string> files = Preferences.getAllDirectories ?
             extension.SelectMany(f => Directory.GetFiles(path, f, SearchOption.AllDirectories)) :
             extension.SelectMany(f => Directory.GetFiles(path, f, SearchOption.TopDirectoryOnly));
