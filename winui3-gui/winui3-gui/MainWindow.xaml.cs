fa16c02d930b315bbc8ba5ff8fda54554caadffb
Az21 <39803478+Az-21@users.noreply.github.com>
2021-09-09 21:57:25 +0530

    🚧 three primary GUI cards
    - Card with button
    - Card with toggle
    - Card with button + text field

--- winui3-gui/winui3-gui/MainWindow.xaml.cs
+++ winui3-gui/winui3-gui/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using Microsoft.UI.Xaml;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -24,9 +11,19 @@ namespace winui3_gui {
         public MainWindow() {
             this.InitializeComponent();
         }
-
-        private void myButton_Click(object sender, RoutedEventArgs e) {
-            myButton.Content = "Clicked";
+        /*
+        private void Control2_Checked(object sender, RoutedEventArgs e) {
+            checkSetting.Content = "I am checked";
         }
+
+        private void Control2_Unchecked(object sender, RoutedEventArgs e) {
+            checkSetting.Content = "I am unchecked";
+        }
+
+        // XAML
+        <CheckBox x:Name="checkSetting" Grid.Row="0" Grid.Column="0" Content="Crawl subfolders"
+                Checked="Control2_Checked"
+                Unchecked="Control2_Unchecked" />
+        */
     }
 }
